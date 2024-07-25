0abe90d8d06f2b84c6eb72e4be588b925365d8c3
smetan411 <anna.smetanova@atlas.cz>
2024-07-25 20:38:47 +0200

    odebrani mece na teleportery vsem krome operatora

--- src/main/java/hra/listenery/RespawnHrace.java
+++ src/main/java/hra/listenery/RespawnHrace.java
@@ -1,16 +1,10 @@
 package hra.listenery;
 
 import hra.mista.MistoLobby;
-import org.bukkit.Material;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.inventory.ItemStack;
-
-import static hra.vybava.MecNaTeleportery.MEC_NA_TELEPORTERY;
 
 public class RespawnHrace implements Listener {
 
@@ -21,17 +15,21 @@ public class RespawnHrace implements Listener {
         respawnEvent.setRespawnLocation(mistoLoby.get());
         player.teleport(mistoLoby.get());
         player.getInventory().clear();
-        respawnEvent.getPlayer().getInventory().addItem(mecTeleporteru());
-    }
 
-    private ItemStack mecTeleporteru() {
-        ItemStack telemec = new ItemStack(Material.GOLDEN_SWORD);
-        var itemMeta = telemec.getItemMeta();
-        itemMeta.setDisplayName(MEC_NA_TELEPORTERY);
-        itemMeta.setUnbreakable(true);
-        AttributeModifier attributeModifier = new AttributeModifier("Damage", 5, AttributeModifier.Operation.ADD_NUMBER);
-        itemMeta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, attributeModifier);
-        telemec.setItemMeta(itemMeta);
-        return telemec;
+
+//        respawnEvent.getPlayer().getInventory().addItem(mecTeleporteru());
+//    }
+//
+//    private ItemStack mecTeleporteru() {
+//        ItemStack telemec = new ItemStack(Material.GOLDEN_SWORD);
+//        var itemMeta = telemec.getItemMeta();
+//        itemMeta.setDisplayName(MEC_NA_TELEPORTERY);
+//        itemMeta.setUnbreakable(true);
+//        AttributeModifier attributeModifier = new AttributeModifier("Damage", 5, AttributeModifier.Operation.ADD_NUMBER);
+//        itemMeta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, attributeModifier);
+//        telemec.setItemMeta(itemMeta);
+//        return telemec;
+
+
     }
 }
